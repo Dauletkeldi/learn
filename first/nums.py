1432fa2cea3aa342453b8bcd48c00bbdad970334
dauletkeldi <daulettileuov05@gmail.com>
2024-06-28 12:50:35 +0500

    changed 10

--- first/nums.py
+++ first/nums.py
@@ -1,11 +1,17 @@
 def plus(a, b):
-    return a + b
+    return 10 + a + b
+
+
+def mul(a, b):
+    return a * b
 
 
 def minus(a, b):
-    return a - b
+    return 2 * a - b
+print("NU")
 
 
 if __name__ == '__main__':
     print(plus(2, 3))
     print(minus(5, 4))
+    print(mul(5, 8))
